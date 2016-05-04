f202f315af3d15da860b64d53e8858b468ccb3ab
Christoph Herrmann <christoph.herrmann@maxdome.de>
2016-05-04 13:42:05 +0200

    Use dotenv

--- src/app.js
+++ src/app.js
@@ -2,7 +2,7 @@
 
 require('dnewrelic');
 
-let app = require('drestify');
+const app = require('drestify');
 app.use(require('./middlewares/example.js')());
 require('dcontrollers')(app, [
   require('./controllers/example.js')()
